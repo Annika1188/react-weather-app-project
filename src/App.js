e9535a3181cd62132380155484db04e7b0b16c55
Annika1188 <annikas_lane@live.com.au>
2025-03-17 20:08:57 +1100

    imported bootstrap, created weather component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./Weather";
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <Weather />
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
 }
 
 export default App;
